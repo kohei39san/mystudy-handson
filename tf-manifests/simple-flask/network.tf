b382ba0f82619215a5180b00edce2ad1fd1089f2
kohei39san <shi8teikoh@gmail.com>
2023-07-24 08:30:36 +0900

    fix security group

--- tf-manifests/simple-flask/network.tf
+++ tf-manifests/simple-flask/network.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "http_sg" {
   description = "allow http"
   vpc_id = module.common_resources.test_vpc.id
   ingress {
-    from_port = 0
+    from_port = 80
     to_port = 80
     protocol = "tcp"
     cidr_blocks = ["${chomp(data.http.client_global_ip.response_body)}/32"]
