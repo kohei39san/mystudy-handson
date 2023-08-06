a1e163f1145dbcabb4f73f5aee096fcf8d9f1836
kohei39san <shi8teikoh@gmail.com>
2023-08-06 15:33:48 +0900

    fix git-clone.sh

--- scripts/git-clone.sh
+++ scripts/git-clone.sh
@@ -4,7 +4,7 @@ echo "install git"
 sudo yum install -y git
 
 echo "git clone"
-if [[ -z ${COMMIT_ID} ]];
+if [[ -z ${COMMIT_ID} ]]; then
   COMMIT_ID=main
 fi
 cd /tmp
@@ -12,3 +12,4 @@ git clone https://github.com/kohei39san/mystudy-handson.git
 cd mystudy-handson
 git checkout ${COMMIT_ID}
 git submodule update --init
+chmod -R +x scripts
