abef484c2c3fee9165fc2439ca406a4f499610b2
kohei39san <137408577+kohei39san@users.noreply.github.com>
2025-05-04 05:45:30 +0000

    [create-pull-request] automated change

--- 014.bedrock-webcrawler/bedrock.tf
+++ 014.bedrock-webcrawler/bedrock.tf
@@ -2,7 +2,7 @@
 resource "aws_cloudformation_stack" "bedrock" {
   name          = "${var.project_name}-bedrock"
   template_body = file("${path.module}/../src/014.bedrock-webcrawler/bedrock-stack.yaml")
-  iam_role_arn      = aws_iam_role.cloudformation.arn
+  iam_role_arn  = aws_iam_role.cloudformation.arn
   capabilities  = ["CAPABILITY_IAM"]
 
   parameters = {
@@ -11,7 +11,7 @@ resource "aws_cloudformation_stack" "bedrock" {
     OpenSearchArn      = aws_opensearch_domain.vector_store.arn
     OpenSearchEndpoint = aws_opensearch_domain.vector_store.endpoint
     AwsRegion          = var.aws_region
-    CrawlingUrl       = var.crawling_url
+    CrawlingUrl        = var.crawling_url
   }
 
   depends_on = [
