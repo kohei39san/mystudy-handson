d0c3c41f8066b78499aeca4978cbffa933bd0ba6
kohei39san <shi8teikoh@gmail.com>
2025-05-29 20:27:20 +0900

    terraform_required_providersの追加

--- wsl-old/tf-manifests/commons/ec2-eice-tunnel/eice.tf
+++ wsl-old/tf-manifests/commons/ec2-eice-tunnel/eice.tf
@@ -1,3 +1,4 @@
+# tflint-ignore-file: terraform_required_providers
 resource "aws_ec2_instance_connect_endpoint" "eice" {
   subnet_id          = aws_subnet.subnet.id
   security_group_ids = [aws_security_group.sg_eice.id]
