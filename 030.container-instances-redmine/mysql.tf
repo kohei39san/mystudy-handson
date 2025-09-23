1b9fa062d25981378e6b021ea65129ddb409359c
kohei39san <shi8teikoh@gmail.com>
2025-09-23 12:01:49 +0900

    refactor: reorganize MySQL DB system resource configuration for clarity and consistency

--- 030.container-instances-redmine/mysql.tf
+++ 030.container-instances-redmine/mysql.tf
@@ -1,32 +1,17 @@
 # MySQL HeatWave Database System
 resource "oci_mysql_mysql_db_system" "redmine_mysql" {
   compartment_id = var.compartment_id
-
+  shape_name          = "MySQL.Free"
+  subnet_id     = oci_core_subnet.mysql_subnet.id
+  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
   admin_password      = var.mysql_admin_password
   admin_username      = var.mysql_admin_username
-  availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
-  shape_name          = "MySQL.Free"
-
-  subnet_id     = oci_core_subnet.mysql_subnet.id
-  nsg_ids       = [oci_core_network_security_group.mysql_nsg.id]
-  mysql_version = var.mysql_version
-
-  display_name = "redmine-mysql"
+  display_name = "RedmineMySQL"
   description  = "MySQL HeatWave for Redmine application"
-
-  data_storage_size_in_gb = 50
-  hostname_label          = "redmine-mysql"
-
-  backup_policy {
-    is_enabled        = true
-    retention_in_days = 7
-    window_start_time = "02:00-00:00"
+  deletion_policy {
+    automatic_backup_retention = "DELETE"
   }
-
-  maintenance {
-    window_start_time = "SUNDAY 02:00"
-  }
-
+  nsg_ids       = [oci_core_network_security_group.mysql_nsg.id]
   freeform_tags = var.freeform_tags
 }
 
