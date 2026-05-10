931969553c9c0d7041cbd0b7627bd9ab878bbd78
kohei39san <shi8teikoh@gmail.com>
2026-05-10 20:18:07 +0900

    fix: update required Terraform version to 1.5.0 for compatibility

--- 041.gcp-billing-alert/terraform.tf
+++ 041.gcp-billing-alert/terraform.tf
@@ -1,13 +1,11 @@
 terraform {
-  required_version = ">= 1.9.6"
+  required_version = ">= 1.5.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "~> 6.0"
     }
   }
-
-  backend "gcs" {}
 }
 
 provider "google" {
